25dfbbf94b8f3c1e5369033ac664adadf3034439
MarceloMachadoxD <marcelo.ultraje@gmail.com>
2021-07-18 17:48:30 -0300

    Catalogo com ElasticSearch

---
+++ catalogoprodutos/src/test/java/com/github/marcelomachadoxd/catalogoprodutos/CatalogoprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.catalogoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
